156b11d31bf0a826a4c184b26e0205a3d6df44e2
kumarikavita.cts <kumarikavita.cts@gmail.com>
2026-09-11 08:58:16 +0530

    aos animation installed

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import './index.css';
 import App from "./App";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import { ThemeProvider } from "./context/ThemeContext";
 import ScrollToTop from "./components/ScrollToTop";
 import './css/Style.css'
 import './css/Responsive.css'
 import './css/Animation.css'
 
+AOS.init({
+  duration: 1500,
+  once: true,
+  offset: 80,
+  easing: "ease-out",
+  disable: function () {
+    return window.innerWidth < 1200;
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    
-    <ScrollToTop/>
+      <ScrollToTop/>
       <ThemeProvider>
         <App />
       </ThemeProvider>
